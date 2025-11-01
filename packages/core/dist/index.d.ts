c255bb5d2af636238fa035a4be7de0a1c0ba206b
Sliverside <241207981+lmoulanier@users.noreply.github.com>
2025-11-01 15:28:53 +0100

    feat: ignore dist directories

--- packages/core/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { a as PrefixedElementTag, i as ElementFromPrefixedTag, r as ElementAttributes } from "./types-DlSAw10c.js";
-
-//#region src/index.d.ts
-declare function el<K extends PrefixedElementTag>(tag: K, attributes?: ElementAttributes<K> | null, ...children: NonNullable<ElementAttributes<K>['children']>): ElementFromPrefixedTag<K>;
-//#endregion
-export { el };
-//# sourceMappingURL=index.d.ts.map
